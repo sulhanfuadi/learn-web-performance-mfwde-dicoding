4497d4e48262edfbeb10445433d5df54c5a9f8ee
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-04-17 10:31:44 +0700

    add new folder

---
+++ gifToVideo/video-over-gif/build-video.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const FFmpeg = require('fluent-ffmpeg');
+
+/**
+ * Full documentations:
+ * https://github.com/fluent-ffmpeg/node-fluent-ffmpeg#usage
+ */
+const gify = new FFmpeg({
+  source: path.resolve(__dirname, 'src/images/animation-footage.gif'),
+});
+
+gify
+  .clone()
+  .withVideoCodec('libx264')
+  .withFps(25)
+  .toFormat('mp4')
+  .saveToFile(path.resolve(__dirname, 'src/images/animation-footage.mp4'));
+
+gify
+  .clone()
+  .withFps(25)
+  .toFormat('webm')
+  .saveToFile(path.resolve(__dirname, 'src/images/animation-footage.webm'));
