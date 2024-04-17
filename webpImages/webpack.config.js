5b68fa0aa09ef278a5aab5489428adb7b8e44ab8
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-04-17 11:55:11 +0700

    add new folder

--- webpImages/webpack.config.js
+++ webpImages/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
+ 
 module.exports = {
   mode: 'production',
   entry: {
@@ -28,19 +28,28 @@ module.exports = {
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(),
-
     new HtmlWebpackPlugin({
-      filename: 'index.html',
       template: path.resolve(__dirname, 'src/templates/index.html'),
+      filename: 'index.html',
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, 'src/public'),
-          to: path.resolve(__dirname, 'dist/'),
+          to: path.resolve(__dirname, 'dist'),
         },
       ],
+    }),
+    new ImageminWebpWebpackPlugin({
+      config: [
+        {
+          test: /\.(jpe?g|png)/,
+          options: {
+            quality: 50,
+          },
+        },
+      ],
+      overrideExtension: true,
     }),
   ],
 };
